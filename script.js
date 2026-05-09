f634b422fdde6cc8e7ab67ac428f40e8d04f2b39
essemf <mouhamedfaye1482@gmail.com>
2026-05-09 17:14:56 +0000

    theme

--- script.js
+++ script.js
@@ -954,6 +954,41 @@ function envoyerMessagePrive() {
     });
 }
 
+function toggleDarkMode() {
+
+    document.body.classList.toggle("dark-mode");
+
+    if(
+        document.body.classList.contains("dark-mode")
+    ) {
+
+        localStorage.setItem(
+            "theme",
+            "dark"
+        );
+
+    } else {
+
+        localStorage.setItem(
+            "theme",
+            "light"
+        );
+    }
+}
+
+function chargerTheme() {
+
+    let theme =
+        localStorage.getItem("theme");
+
+    if(theme === "dark") {
+
+        document.body.classList.add(
+            "dark-mode"
+        );
+    }
+}
+
 // =====================
 // LOGOUT
 // =====================
